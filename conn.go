a2976247d17a12e897ab8cc1ccb011ba528e6499
zhenghuabin <zhenghuabin@bytedance.com>
2015-03-25 20:09:54 +0800

    add MarkUnreusable to pooled connection
    to make it possible to discard unreusable connections

--- conn.go
+++ conn.go
@@ -2,21 +2,33 @@ package pool
 
 import "net"
 
-// poolConn is a wrapper around net.Conn to modify the the behavior of
+// PoolConn is a wrapper around net.Conn to modify the the behavior of
 // net.Conn's Close() method.
-type poolConn struct {
+type PoolConn struct {
 	net.Conn
-	c *channelPool
+	c          *channelPool
+	unreusable bool
 }
 
 // Close() puts the given connects back to the pool instead of closing it.
-func (p poolConn) Close() error {
+func (p PoolConn) Close() error {
+	if p.unreusable {
+		if p.Conn != nil {
+			p.Conn.Close()
+		}
+		return nil
+	}
 	return p.c.put(p.Conn)
+}
+
+// MarkUnreusable() marks the connection is not reusable.
+func (p *PoolConn) MarkUnreusable() {
+	p.unreusable = true
 }
 
 // newConn wraps a standard net.Conn to a poolConn net.Conn.
 func (c *channelPool) wrapConn(conn net.Conn) net.Conn {
-	p := poolConn{c: c}
+	p := &PoolConn{c: c}
 	p.Conn = conn
 	return p
 }
